e74cd0d18800d912f46fadd9291d3cf45436485c
VEBER Arnaud <arnaud@veber.pw>
2019-01-04 15:49:43 +0100

    feat(dns): veber.pw route53 zones

--- dns.tf
+++ dns.tf
@@ -1,3 +1,12 @@
+# veber.pw
+module "veber_pw-zones" {
+  source = "github.com/VEBERArnaudAWS/tf_module-dns_zones?ref=v0.0.1-alpha.1"
+
+  domain               = "veber.pw"
+  env_names            = "${var.env_names}"
+  env_dns_zones_prefix = "${var.env_dns_zones_prefix}"
+}
+
 # veber.io
 module "veber_io-zones" {
   source = "github.com/VEBERArnaudAWS/tf_module-dns_zones?ref=v0.0.1-alpha.1"
