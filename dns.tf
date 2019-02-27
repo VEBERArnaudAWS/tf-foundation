683b744082e2ad49d7bd14babf2ae9b49ddc303b
VEBER Arnaud <arnaud@veber.pw>
2019-02-27 06:01:01 +0100

    feat(dns): add awsworkshop.paris zones

--- dns.tf
+++ dns.tf
@@ -1,3 +1,14 @@
+# awsworkshop.paris
+module "awsworkshop_paris-zones" {
+  source = "github.com/VEBERArnaudAWS/tf_module-dns_zones?ref=v0.0.1-alpha.2"
+
+  bypass = "${terraform.workspace != "prd" ? "true" : "false"}"
+
+  domain               = "awsworkshop.paris"
+  env_names            = "${var.env_names}"
+  env_dns_zones_prefix = "${var.env_dns_zones_prefix}"
+}
+
 # grantueismo.world
 module "granturismo_world-zones" {
   source = "github.com/VEBERArnaudAWS/tf_module-dns_zones?ref=v0.0.1-alpha.2"
